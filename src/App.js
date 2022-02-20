16ec6c8c08deecf8ef233601eab09d0fbea21660
Anthony Nguyen <anthony33nguyen@gmail.com>
2022-02-20 00:02:18 -0500

    Added Navbar component and adjusted position for it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Home from './components/pages/Home'
+import Upload from './components/pages/Upload'
+import About from './components/pages/About'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className='App'>
+      <Navbar />
+      <Home />
+      <Upload />
+      <About />
     </div>
   );
 }
